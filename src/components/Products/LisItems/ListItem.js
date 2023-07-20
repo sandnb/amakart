24741cc86e3e470ef7371b8b7d01f43806b9f5bf
sandnb <nithinsandilyab@gmail.com>
2023-07-20 18:10:29 +0530

    attached the backend successfully to the frontend

--- src/components/Products/LisItems/ListItem.js
+++ src/components/Products/LisItems/ListItem.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import AddToCart from "../../../assets/icons/add_cart.svg"
+import { useState } from 'react'
 
 
-function ListItem( {data} ) {
+function ListItem( {data, updateItemTitle}   ) {
+    const [counter,setCounter] = useState(0);
+
+    const increaseCounterByOne = () => {
+        setCounter(counter + 1);
+    }
+
+    const decreaseCounterByOne = () => {
+        if(counter === 0){
+            return;
+        }
+        setCounter(counter -1);
+    }
    
   return (
     <div>
@@ -19,10 +32,24 @@ function ListItem( {data} ) {
                     <h3>{data.title}</h3>
                 </div>
             </div>
-            <button className={'cart-add'}>
+            <button onClick={() => updateItemTitle(data.id)}>Update the title</button>
+            {
+                counter < 1 ?
+
+            <button className={'cart-add'} onClick={increaseCounterByOne}>
                 <span>Add to Cart</span>
                 <img src={AddToCart} alt="cart-icon"/>
-            </button>
+            </button> 
+             :
+            <div className='cart-addon'>
+                <button onClick = {decreaseCounterByOne}><span>-</span></button>
+                <span>{counter}</span>
+                <button onClick={increaseCounterByOne}><span>+</span></button>
+            </div>
+
+            }
+           
+            
         </div>
     </div>
   )
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,41 +1,54 @@
-
+import { useState, useEffect } from 'react'
 import ListItem from './LisItems/ListItem'
-
+import axios from 'axios';
 
 function Products() {
-    const items =[
-        {
-            id:0,
-            discountedPrice: 340,
-            price: 450,
-            title: "Title of the item",
-            thumbnail: "placeholder.png",
+    const [items,setItems]  = useState([]);
 
-        },
-        {
-            id:1,
-            discountedPrice: 450,
-            price: 4500,
-            title: "Gulab-Jaamunn",
-            thumbnail: "placeholder.png",
-
-        },
-        {
-            id:2,
-            discountedPrice: 300,
-            price: 457,
-            title: "Basmathi-Rice",
-            thumbnail: "placeholder.png",
-
+    useEffect(()=>{
+        async function fetchItems(){
+            try {
+                        const response = await  axios.get(`https://ekart-b2904-default-rtdb.firebaseio.com/items.json`)
+                        const data = response.data;
+                        const transformedData = data.map((item,index)=>{
+                                    return{
+                                        ...item,
+                                        id:index,
+                                    }
+                    })
+                    setItems(transformedData);
+            }
+            catch(error){
+                console.log(error);
+            }
+          
         }
-    ]    
+       fetchItems();
+    },[])
+
+    const updateItemTitle = async (itemId) => {
+        try{
+            let title = `Updated title #item-${itemId}`
+            await axios.patch(`https://ekart-b2904-default-rtdb.firebaseio.com/items/${itemId}.json`,{
+                title: title
+        })
+           let data = [...items]
+           let index =  data.findIndex(e => e.id === itemId);
+           data[index]['title'] = title
+
+           setItems(data);
+        }
+       catch(error){
+        console.log(error);
+       }
+    }
 
   return (
     <div className={'product-list'}>
         <div className={'product-list--wrapper'}>
-            <ListItem data={items[0]}/>
-            <ListItem data={items[1]}/>
-            <ListItem data={items[2]}/>
+            {items.map(item=>{
+                return(<ListItem key={item.id} data={item} updateItemTitle={updateItemTitle} />)
+            })}
         </div>
     </div>
   )
